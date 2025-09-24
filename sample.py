8bf3de5848c792c33a7b9e651dd67f076ecfcab8
Zade Viggers <zade@viggers.net>
2025-09-24 15:32:00 +1200

    Cooked wip

--- sample.py
+++ sample.py
@@ -9,6 +9,11 @@ from contextlib import nullcontext
 import torch
 import tiktoken
 from model import GPTConfig, GPT
+import seaborn as sns
+import matplotlib as plt
+
+# Apply seaborn default theme
+sns.set_theme()
 
 # -----------------------------------------------------------------------------
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
@@ -18,10 +23,11 @@ num_samples = 10 # number of samples to draw
 max_new_tokens = 500 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
-seed = 1337
+seed = 42069
 device = torch.device("mps") # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'float16' # 'bfloat16' or 'float16'
-compile = False # use PyTorch 2.0 to compile the model to be faster
+show_probs = False # Set to True to see chart of top 10 tokens each iteration
+compile = True # use PyTorch 2.0 to compile the model to be faster
 exec(open('configurator.py').read()) # overrides from command line or config file
 # -----------------------------------------------------------------------------
 
@@ -79,6 +85,10 @@ else:
 if start.startswith('FILE:'):
     with open(start[5:], 'r', encoding='utf-8') as f:
         start = f.read()
+
+# Make newlines work
+start = start.replace("\\n", "\n")
+
 start_ids = encode(start)
 x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
 
@@ -86,6 +96,38 @@ x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
 with torch.no_grad():
     with ctx:
         for k in range(num_samples):
-            y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
-            print(decode(y[0].tolist()))
-            print('---------------')
+            if show_probs:
+                generator = model.generate_generator(x, max_new_tokens, temperature=temperature, top_k=top_k)
+                
+                print("\n\nCompletetion including prompt:\n" + start, end="")
+                for generation in generator:
+                    token_y = generation["iteration_token"]
+                    probs = generation["iteration_probability_dist"]
+
+                    token = decode(token_y[0].tolist())
+
+                    # Cursed loop to get the top 10 probs & indexes
+                    # [index, prob][]
+                    top_10_probs_ys = [[-1, 0.0] for i in range(10)]
+
+                    for prob, i in enumerate(probs.tolist()):
+                        if prob > 0.0:
+                            print(prob, decode([i]))
+
+                        for j, tprob in top_10_probs_ys:
+                            if prob > tprob:
+                                top_10_probs_ys[j] = [i, prob]
+                                break
+
+                    print(top_10_probs_ys)
+                        
+                    # [token, prob, is_generated]
+                    top_10_probs = [[decode([i]), prob, i == token_y[0][0]] for i, prob in top_10_probs_ys]
+                    print(top_10_probs)
+                    
+                    # plt.pyplot.show() 
+                print("\n\n\nDone.")
+            else:
+                y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
+                print(decode(y[0].tolist()))
+                print('---------------')
