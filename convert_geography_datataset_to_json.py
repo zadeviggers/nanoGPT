38a569417133ca31f22099561b24249bf7c03d05
Zade Viggers <zade@viggers.net>
2025-10-01 15:40:07 +1300

    Select 10 random

--- convert_geography_datataset_to_json.py
+++ convert_geography_datataset_to_json.py
@@ -2,6 +2,7 @@
 
 import csv
 import json
+import random
 
 in_file = "Geography_11th_Cleaned.csv"
 out_file = "eval_data.json"
@@ -20,5 +21,9 @@ with open(out_file, 'w') as jsonfile:
             question = row[2]
             answer = row[3]
             rows.append({"prompt": question, "response": answer})
+    
+    # Take 10 random items
+    random.shuffle(rows)
+    rows = rows[:10]
     json.dump(rows, jsonfile)
 
