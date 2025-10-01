0b0d7c9c16275142bad2fac8f211a8105748f551
Zade Viggers <zade@viggers.net>
2025-10-01 14:46:37 +1300

    Geog data

---
+++ convert_geography_datataset_to_json.py
@@ -0,0 +1,24 @@
+#! python3
+
+import csv
+import json
+
+in_file = "Geography_11th_Cleaned.csv"
+out_file = "eval_data.json"
+
+with open(out_file, 'w') as jsonfile:
+    rows = []
+    with open(in_file) as csvfile:
+        reader = csv.reader(csvfile)
+        first_row = True
+        for row in reader:
+            if first_row:
+                # Skip headers
+                first_row = False
+                continue
+
+            question = row[2]
+            answer = row[3]
+            rows.append({"prompt": question, "response": answer})
+    json.dump(rows, jsonfile)
+
